dfd44896de97f10ff09d26441f76ed47d433f6f7
AnaLu1za <aninha141108@gmail.com>
2024-08-08 12:27:34 -0300

    - Implementa função para excluir usuário da lista e remover linha correspondente da tabela, utiliza método splice para remover elemento do array e deleteRow para remover linha da tabela. - Implementa função para editar usuário da lista e atualizar campo de entrada com o nome selecionado. utiliza método splice para remover elemento do array e atualizar valor do input.

--- controller.js
+++ controller.js
@@ -33,7 +33,20 @@ function salvarUser(){
 function criarLista(){
     let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
     for(let i = 0; i <= (dadosLista.length -1); i++){ // A propriedade length tem como responsabilidade retornar a quantidade de caracteres de uma string ou o tamanho de um array. Caso a string ou o array esteja vazio, é retornado o valor 0.
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td></td></tr>"; // concatenar dados lista na posção i 
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td> <button onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button> </td></tr>"; // concatenar dados lista na posção i 
         document.getElementById('tabela').innerHTML = tabela; // coloca conteudo de variavel tabela em pagina html
     }
+}
+
+//Função para excluir nome de lista 
+function excluir(i){ // apaga um elemento de posição i da array
+    dadosLista.splice((i - 1), 1); //slice: extrai uma parte de uma string e a retorna como uma nova string, sem modificar a string original.
+    document.getElementById('tabela').deleteRow(i); //deleteRow = apagar linha 
+}
+
+//Função para editar nome da lista 
+function editar(i){
+    document.getElementById("nomeUser").value = dadosLista 
+    [(i - 1)];
+    dadosLista.splice(dadosLista[(i -1)], 1);
 }
